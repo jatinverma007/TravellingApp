b2645c38d6eabfced7a85173ba25212c77ca60e9
jatin <jatin.v1997@gmail.com>
2016-07-22 13:30:33 +0530

    Updating Constraints fro gmail and facebok button

---
+++ Travelling/DemoViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DemoViewController.swift
+//  Travelling
+//
+//  Created by Jatin Verma on 7/22/16.
+//  Copyright © 2016 Jatin Verma. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
